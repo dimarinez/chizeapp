533ae0b3b24c27e8683e94ad33d8c439eca263d5
Dillon Marinez <dimarinez@yahoo.com>
2023-08-06 11:54:54 -0700

    adds port variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config(); 
-// const port = 3000; // You can use any port number you prefer
+const port = process.env.PORT || 3000; // You can use any port number you prefer
 
 // APNs configuration
 const apn = require('apn');
@@ -14,40 +14,37 @@ const apnProvider = new apn.Provider({
   production: true, // Change to true for production environment
 });
 
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
 app.use(express.json());
 
 // Endpoint to send push notification
 app.post('/sendNotificationRequest', (req, res) => {
-    const deviceToken = req.body.record.receiver_deviceToken; 
+    const deviceToken = req.body.record.receiver_deviceToken;
 
-  if (!deviceToken) {
-    return res.status(400).json({ error: 'Device token is required' });
-  }
+    if (!deviceToken) {
+        return res.status(400).json({ error: 'Device token is required' });
+    }
 
-  const notification = new apn.Notification();
-  notification.topic = 'org.reactjs.native.example.CoffeeMeet';
-  notification.title = 'Sample Title';
-  notification.subtitle = 'Sample Subtitle';
-  notification.body = 'Sample body of the notification';
-  notification.sound = 'default';
+    const notification = new apn.Notification();
+    notification.topic = 'org.reactjs.native.example.CoffeeMeet';
+    notification.title = 'Sample Title';
+    notification.subtitle = 'Sample Subtitle';
+    notification.body = 'Sample body of the notification';
+    notification.sound = 'default';
   
   // Send the notification
-  apnProvider.send(notification, deviceToken)
+    apnProvider.send(notification, deviceToken)
     .then(result => {
-      console.log('Notification sent:', result);
-      res.status(200).json({ success: true });
+        console.log('Notification sent:', result);
+        res.status(200).json({ success: true });
     })
     .catch(error => {
-      console.error('Error sending notification:', error);
-      res.status(500).json({ error: 'Error sending notification' });
+        console.error('Error sending notification:', error);
+        res.status(500).json({ error: 'Error sending notification' });
     });
 });
 
-apnProvider.on('error', (error) => {
-    console.error('APNs Provider Error:', error);
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
+  
