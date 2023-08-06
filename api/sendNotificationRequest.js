cb1f37dbd831c5644a9a884171ea5afd13ec8b78
Dillon Marinez <dimarinez@yahoo.com>
2023-08-06 14:26:57 -0700

    adds production false

--- api/sendNotificationRequest.js
+++ api/sendNotificationRequest.js
@@ -10,11 +10,13 @@ const apnProvider = new apn.Provider({
     keyId: 'ZURVGBDD3L', // Replace with your Key ID
     teamId: '98WJP6S25J', // Replace with your Team ID
   },
-  production: true, // Change to true for production environment
+  production: false, // Change to true for production environment
 });
 
 module.exports = async (req, res) => {
   const deviceToken = req.body.record.receiver_deviceToken;
+
+  console.log(deviceToken);
 
   if (!deviceToken) {
     return res.status(400).json({ error: 'Device token is required' });
@@ -30,6 +32,7 @@ module.exports = async (req, res) => {
   try {
     const result = await apnProvider.send(notification, deviceToken);
     console.log('Notification sent:', result);
+    console.log(result.failed[0].response);
     res.status(200).json({ success: true });
   } catch (error) {
     console.error('Error sending notification:', error);
