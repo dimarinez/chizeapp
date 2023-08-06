5950f6131931cbecc819542c7dead50d1cff39a5
Dillon Marinez <dimarinez@yahoo.com>
2023-08-06 11:38:55 -0700

    Adds json and record

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
-import express from 'express';
-
+const express = require('express');
 const app = express();
+require('dotenv').config(); 
+// const port = 3000; // You can use any port number you prefer
 
-// enable JSON body parser
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// APNs configuration
+const apn = require('apn');
+const apnProvider = new apn.Provider({
+  token: {
+    key: process.env.APNS, // Replace with your APNs Authentication Key path
+    keyId: 'ZURVGBDD3L', // Replace with your Key ID
+    teamId: '98WJP6S25J', // Replace with your Team ID
+  },
+  production: true, // Change to true for production environment
 });
 
-export default app;
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+app.use(express.json());
+
+// Endpoint to send push notification
+app.post('/sendNotificationRequest', (req, res) => {
+    const deviceToken = req.body.record.receiver_deviceToken; 
+
+  if (!deviceToken) {
+    return res.status(400).json({ error: 'Device token is required' });
+  }
+
+  const notification = new apn.Notification();
+  notification.topic = 'org.reactjs.native.example.CoffeeMeet';
+  notification.title = 'Sample Title';
+  notification.subtitle = 'Sample Subtitle';
+  notification.body = 'Sample body of the notification';
+  notification.sound = 'default';
+  
+  // Send the notification
+  apnProvider.send(notification, deviceToken)
+    .then(result => {
+      console.log('Notification sent:', result);
+      res.status(200).json({ success: true });
+    })
+    .catch(error => {
+      console.error('Error sending notification:', error);
+      res.status(500).json({ error: 'Error sending notification' });
+    });
+});
+
+apnProvider.on('error', (error) => {
+    console.error('APNs Provider Error:', error);
+});
