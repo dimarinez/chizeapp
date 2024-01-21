01dbc4cd733e84c54b410ae27cb7dd8171d10c20
Dillon Marinez <dimarinez@yahoo.com>
2024-01-20 19:09:49 -0800

    adds privacy and terms with email

--- client/src/components/PrivacyPolicy.js
+++ client/src/components/PrivacyPolicy.js
@@ -3,8 +3,186 @@ import React from 'react';
 const PrivacyPolicy = () => {
   return (
     <div>
-      <h1>Privacy Policy</h1>
-      <p>This page outlines the privacy policy of our application.</p>
+        <h1>Privacy Policy</h1>
+        <p>
+        Privacy Policy for Chize
+
+        Last updated: January 20, 202
+
+        Thank you for choosing to be part of our community at Chize App ("Company", "we", "us", or "our"). We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about this privacy policy or our practices with regard to your personal information, please contact us at feedback@chizeapp.com.
+
+        This privacy policy describes how we might use your information if you:
+
+        Download and use our mobile application — Chize
+        Engage with us in other related ways ― including any sales, marketing, or events
+        Please read this privacy policy carefully, as it will help you understand what we do with the information that we collect.
+
+        TABLE OF CONTENTS
+
+WHAT INFORMATION DO WE COLLECT?
+HOW DO WE USE YOUR INFORMATION?
+WILL YOUR INFORMATION BE SHARED WITH ANYONE?
+DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?
+HOW DO WE HANDLE YOUR SOCIAL LOGINS?
+IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?
+WHAT IS OUR STANCE ON THIRD-PARTY WEBSITES?
+HOW LONG DO WE KEEP YOUR INFORMATION?
+HOW DO WE KEEP YOUR INFORMATION SAFE?
+DO WE COLLECT INFORMATION FROM MINORS?
+WHAT ARE YOUR PRIVACY RIGHTS?
+CONTROLS FOR DO-NOT-TRACK FEATURES
+DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?
+DO WE MAKE UPDATES TO THIS POLICY?
+HOW CAN YOU CONTACT US ABOUT THIS POLICY?
+1. WHAT INFORMATION DO WE COLLECT?
+
+Personal information you disclose to us
+In Short: We collect personal information that you provide to us.
+
+We collect personal information that you voluntarily provide to us when you register on the App, express an interest in obtaining information about us or our products and services, when you participate in activities on the App, or otherwise when you contact us.
+
+The personal information that we collect depends on the context of your interactions with us and the App, the choices you make, and the products and features you use. The personal information we collect may include the following:
+
+Personal Information Provided by You. We collect names; phone numbers; location data; photos; email addresses; usernames; passwords; contact or authentication data; and other similar information.
+
+In Short: Some information — such as your Internet Protocol (IP) address and/or browser and device characteristics — is collected automatically when you visit our App.
+
+We automatically collect certain information when you visit, use, or navigate the App. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use our App, and other technical information. This information is primarily needed to maintain the security and operation of our App, and for our internal analytics and reporting purposes.
+
+2. HOW DO WE USE YOUR INFORMATION?
+
+In Short: We process your information for purposes based on legitimate business interests, the fulfillment of our contract with you, compliance with our legal obligations, and/or your consent.
+
+We use personal information collected via our App for a variety of business purposes described below. We process your personal information for these purposes in reliance on our legitimate business interests, in order to enter into or perform a contract with you, with your consent, and/or for compliance with our legal obligations. We indicate the specific processing grounds we rely on next to each purpose listed below.
+
+We use the information we collect or receive:
+
+To facilitate account creation and logon process.
+To send administrative information to you.
+To protect our Services.
+To enforce our terms, conditions, and policies for business purposes, to comply with legal and regulatory requirements, or in connection with our contract.
+To respond to legal requests and prevent harm.
+
+3. WILL YOUR INFORMATION BE SHARED WITH ANYONE?
+
+In Short: We only share information with your consent, to comply with laws, to provide you with services, to protect your rights, or to fulfill business obligations.
+
+We may process or share your data that we hold based on the following legal basis:
+
+Consent: We may process your data if you have given us specific consent to use your personal information for a specific purpose.
+Legitimate Interests: We may process your data when it is reasonably necessary to achieve our legitimate business interests.
+Performance of a Contract: Where we have entered into a contract with you, we may process your personal information to fulfill the terms of our contract.
+Legal Obligations: We may disclose your information where we are legally required to do so in order to comply with applicable law, governmental requests, a judicial proceeding, court order, or legal process, such as in response to a court order or a subpoena (including in response to public authorities to meet national security or law enforcement requirements).
+Vital Interests: We may disclose your information where we believe it is necessary to investigate, prevent, or take action regarding potential violations of our policies, suspected fraud, situations involving potential threats to the safety of any person, and illegal activities, or as evidence in litigation in which we are involved.
+4. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?
+
+In Short: We may use cookies and other tracking technologies to collect and store your information.
+
+We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store information. Specific information about how we use such technologies and how you can refuse certain cookies is set out in our Cookie Policy.
+
+5. HOW DO WE HANDLE YOUR SOCIAL LOGINS?
+
+In Short: If you choose to register or log in to our services using a social media account, we may have access to certain information about you.
+
+Our App offers you the ability to register and login using your third-party social media account details (like your Facebook or Twitter logins). Where you choose to do this, we will receive certain profile information about you from your social media provider. The profile information we receive may vary depending on the social media provider concerned, but will often include your name, email address, friends list, profile picture as well as other information you choose to make public on such social media platform.
+
+We will use the information we receive only for the purposes that are described in this privacy policy or that are otherwise made clear to you on the App. Please note that we do not control, and are not responsible for, other uses of your personal information by your third-party social media provider. We recommend that you review their privacy policy to understand how they collect, use, and share your personal information, and how you can set your privacy preferences on their sites and apps.
+
+6. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?
+
+In Short: We may transfer, store, and process your information in countries other than your own.
+
+If you are a resident in the European Economic Area (EEA) or United Kingdom (UK), then these countries may not necessarily have data protection laws or other similar laws as comprehensive as those in your country. We will, however, take all necessary measures to protect your personal information in accordance with this privacy policy and applicable law.
+
+7. WHAT IS OUR STANCE ON THIRD-PARTY WEBSITES?
+
+In Short: We are not responsible for the safety of any information that you share with third-party providers who advertise, but are not affiliated with, our app.
+
+The App may contain advertisements from third parties that are not affiliated with us and which may link to other websites, online services, or mobile applications. We cannot guarantee the safety and privacy of data you provide to any third parties. Any data collected by third parties is not covered by this privacy policy. We are not responsible for the content or privacy and security practices and policies of any third parties, including other websites, services, or applications that may be linked to or from the App. You should review the policies of such third parties and contact them directly to respond to your questions.
+
+8. HOW LONG DO WE KEEP YOUR INFORMATION?
+
+In Short: We keep your information for as long as necessary to fulfill the purposes outlined in this privacy policy unless otherwise required by law.
+
+We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy policy, unless a longer retention period is required or permitted by law (such as tax, accounting, or other legal requirements). No purpose in this policy will require us keeping your personal information for longer than the period of time in which users have an account with us.
+
+When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymize such information, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.
+
+9. HOW DO WE KEEP YOUR INFORMATION SAFE?
+
+In Short: We aim to protect your personal information through a system of organizational and technical security measures.
+
+We have implemented appropriate technical and organizational security measures designed to protect the security of any personal information we process. However, despite our safeguards and efforts to secure your information, no electronic transmission over the Internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorized third parties will not be able to defeat our security and improperly collect, access, steal, or modify your information. Although we will do our best to protect your personal information, transmission of personal information to and from our App is at your own risk. You should only access the App within a secure environment.
+
+10. DO WE COLLECT INFORMATION FROM MINORS?
+
+In Short: We do not knowingly collect data from or market to children under the age of 18.
+
+We do not knowingly solicit data from or market to children under the age of 18. By using the App, you represent that you are at least 18 or that you are the parent or guardian of such a minor and consent to such minor dependent’s use of the App. If we learn that personal information from users less than 18 years of age has been collected, we will deactivate the account and take reasonable measures to promptly delete such data from our records. If you become aware of any data we may have collected from children under age 18, please contact us at feedback@chizeapp.com.
+
+11. WHAT ARE YOUR PRIVACY RIGHTS?
+
+In Short: You may review, change, or terminate your account at any time.
+
+If you are a resident in the EEA or UK and you believe we are unlawfully processing your personal information, you also have the right to complain to your local data protection supervisory authority. You can find their contact details here: http://ec.europa.eu/justice/data-protection/bodies/authorities/index_en.htm.
+
+If you are a resident in Switzerland, the contact details for the data protection authorities are available here: https://www.edoeb.admin.ch/edoeb/en/home.html.
+
+If you have questions or comments about your privacy rights, you may email us at feedback@chizeapp.com.
+
+Account Information
+If you would at any time like to review or change the information in your account or terminate your account, you can:
+
+Log in to your account settings and update your user account.
+Contact us using the contact information provided.
+Upon your request to terminate your account, we will deactivate or delete your account and information from our active databases. However, we may retain some information in our files to prevent fraud, troubleshoot problems, assist with any investigations, enforce our Terms of Use, and/or comply with applicable legal requirements.
+
+Cookies and similar technologies: Most Web browsers are set to accept cookies by default. If you prefer, you can usually choose to set your browser to remove cookies and to reject cookies. If you choose to remove cookies or reject cookies, this could affect certain features or services of our App.
+
+Opting out of email marketing: You can unsubscribe from our marketing email list at any time by clicking on the unsubscribe link in the emails that we send or by contacting us using the details provided below. You will then be removed from the marketing email list – however, we may still communicate with you, for example, to send you service-related emails that are necessary for the administration and use of your account, to respond to service requests, or for other non-marketing purposes. To otherwise opt-out, you may:
+
+Access your account settings and update your preferences.
+Contact us using the provided contact information.
+12. CONTROLS FOR DO-NOT-TRACK FEATURES
+
+Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track ("DNT") feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. At this stage no uniform technology standard for recognizing and implementing DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy policy.
+
+13. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?
+
+In Short: Yes, if you are a resident of California, you are granted specific rights regarding access to your personal information.
+
+California Civil Code Section 1798.83, also known as the "Shine The Light" law, permits our users who are California residents to request and obtain from us, once a year and free of charge, information about categories of personal information (if any) we disclosed to third parties for direct marketing purposes and the names and addresses of all third parties with which we shared personal information in the immediately preceding calendar year. If you are a California resident and would like to make such a request, please submit your request in writing to us using the contact information provided below.
+
+If you are under 18 years of age, reside in California, and have a registered account with the App, you have the right to request removal of unwanted data that you publicly post on the App. To request removal of such data, please contact us using the contact information provided below and include the email address associated with your account and a statement that you reside in California. We will make sure the data is not publicly displayed on the App, but please be aware that the data may not be completely or comprehensively removed from all our systems (e.g., backups, etc.).
+
+CCPA Privacy Notice
+The California Code of Regulations defines a "resident" as:
+(1) every individual who is in the State of California for other than a temporary or transitory purpose and
+(2) every individual who is domiciled in the State of California who is outside the State for a temporary or transitory purpose
+All other individuals are defined as "non-residents."
+
+If this definition of "resident" applies to you, we must adhere to certain rights and obligations regarding your personal information.
+
+What categories of personal information do we collect?
+We have collected the following categories of personal information in the past twelve (12) months:
+
+What are your privacy rights?
+If you are a resident of California, you are granted specific rights regarding access to your personal information. California Civil Code Section 1798.83 permits you to request information regarding the disclosure of your personal information by Chize to third parties for the third parties' direct marketing purposes. To request such information, please contact us using the contact information provided below.
+
+14. DO WE MAKE UPDATES TO THIS POLICY?
+
+In Short: Yes, we will update this policy as necessary to stay compliant with relevant laws.
+
+We may update this privacy policy from time to time. The updated version will be indicated by an updated "Revised" date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy policy, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy policy frequently to be informed of how we are protecting your information.
+
+15. HOW CAN YOU CONTACT US ABOUT THIS POLICY?
+
+If you have questions or comments about this policy, you may email us at feedback@chizeapp.com.
+
+HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?
+
+Based on the laws of some countries, you may have the right to request access to the personal information we collect from you, change that information, or delete it. To request to review, update, or delete your personal information, please send an email to feedback@chizeapp.com.
+        </p>
     </div>
   );
 };
